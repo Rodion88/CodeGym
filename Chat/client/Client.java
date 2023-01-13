7057d7e9626cd916bc75e60f416b3ad725516d6e
Rodion88 <122551274+Rodion88@users.noreply.github.com>
2023-01-13 09:23:12 +0100

    Update Client.java

--- Chat/client/Client.java
+++ Chat/client/Client.java
@@ -151,60 +151,8 @@ public class Client {
         }
     }
 
-	}
-
-	public static void main(String[] args) {
-		Client client = new Client();
-		client.run();
-	}
-
-	public class SocketThread extends Thread{
-		protected void processIncomingMessage(String message){
-			ConsoleHelper.writeMessage(message);
-		}
-		protected void informAboutAddingNewUser(String userName){
-			ConsoleHelper.writeMessage(userName + " присоединился к чату");
-		}
-		protected void informAboutDeletingNewUser(String userName){
-			ConsoleHelper.writeMessage(userName + " покинул чат");
-		}
-		protected void notifyConnectionStatusChanged(boolean clientConnected){
-			Client.this.clientConnected = clientConnected;
-			synchronized (Client.this){
-				Client.this.notify();
-			}
-		}
-		protected void clientHandshake() throws IOException, ClassNotFoundException{
-			while (true){
-				Message message = connection.receive();
-				if(message.getType() == MessageType.NAME_REQUEST){
-					connection.send(new Message(MessageType.USER_NAME, getUserName()));
-				} else if (message.getType() == MessageType.NAME_ACCEPTED) {
-					notifyConnectionStatusChanged(true);
-					break;
-				}else throw new IOException("Unexpected MessageType");
-			}
-		}
-		protected void clientMainLoop() throws IOException, ClassNotFoundException{
-			while (true){
-				Message message = connection.receive();
-				if(message.getType() == MessageType.TEXT){
-					processIncomingMessage(message.getData());
-				} else if (message.getType() == MessageType.USER_ADDED) {
-					informAboutAddingNewUser(message.getData());
-				} else if (message.getType() == MessageType.USER_REMOVED) {
-					informAboutDeletingNewUser(message.getData());
-				}else throw new IOException("Unexpected MessageType");
-			}
-		}
-		public void run(){
-			try(Socket socket = new Socket(getServerAddress(), getServerPort())){
-				Client.this.connection = new Connection(socket);
-				clientHandshake();
-				clientMainLoop();
-			}catch (IOException | ClassNotFoundException e){
-				notifyConnectionStatusChanged(false);
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 }
